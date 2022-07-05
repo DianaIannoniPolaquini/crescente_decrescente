08201aad381ad483788d60af46255e810ac50678
DianaIannoniPolaquini <108705643+DianaIannoniPolaquini@users.noreply.github.com>
2022-07-04 23:26:42 -0300

    primeiro commit

---
+++ CRESCENTE_DECRESCENTE/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CRESCENTE_DECRESCENTE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.WriteLine("Entre com dois números inteiros" +
+                "separados por espaço: ");
+            string[] vet = Console.ReadLine().Split(' ');
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+
+            while (x != y)
+            {
+                if (x > y)
+                {
+                    Console.WriteLine("Decrescente!");
+                }
+                else
+                {
+                    Console.WriteLine("Crecente");
+                }
+                vet = Console.ReadLine().Split(' ');
+                x = int.Parse(vet[0]);
+                y = int.Parse(vet[1]);
+
+            }
+
+
+        }
+    }
+}
